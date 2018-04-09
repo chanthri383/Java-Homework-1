8bff718987abbcaad67b2d4b10657ac737932f1e
chanthri383 <36742658+chanthri383@users.noreply.github.com>
2018-04-09 16:52:10 -0500

    Update Mob.java

--- Mob.java
+++ Mob.java
@@ -1,1 +1,18 @@
-// Mob.java
+import "Npc.java";
+
+ public class Mob extends Npc {
+  private String classType;
+  private boolean questMob;
+   
+  public Mob() {
+    classType = null;
+    questMob = false;
+  }
+   
+  public(String type_, String name_, char gender_, int hitPoints_, String zone_,String classType_, boolean questMob_)
+  {
+    super(type_,name_,gender_,hitPoints_,zone_,classType_,questMob_);
+    classType = classType_;
+    questMob = questMob_;
+  }
+

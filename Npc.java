8d329164200c9c1cb26e5426825dd00f23884730
chanthri383 <36742658+chanthri383@users.noreply.github.com>
2018-04-09 16:30:20 -0500

    Update Npc.java

--- Npc.java
+++ Npc.java
@@ -1,1 +1,15 @@
 
+public class Npc extends Character {
+  protected String zone;
+  
+  public Npc() {
+    zone = null;
+  }
+  public Npc(String type_, String name_, char gender_, int hitPoints_, String zone_)
+  {
+    super(type_, name_, gender_, hitPoints_, zone_);
+    zone = zone;
+  }
+  public String toString()
+  {
+    System.out.print

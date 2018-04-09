d2b3b03319eb6ae4fc7350428e874ad51638a37e
chanthri383 <36742658+chanthri383@users.noreply.github.com>
2018-04-09 15:50:27 -0500

    Update Character.java

--- Character.java
+++ Character.java
@@ -1,1 +1,14 @@
+public class Character {
+  protected String type, name;
+  protected char gender;
+  protected int hitPoints;
+  
+  public character() {
+    type = null;
+    name = null; 
+    gender = '\0';
+    hitPoints = 0;
+  }
+  
+  
 

a5d94f5b1fe272a7bce37547b9a880de969f8d61
chanthri383 <36742658+chanthri383@users.noreply.github.com>
2018-04-10 18:00:47 -0500

    Update Townsperson.java

--- Townsperson.java
+++ Townsperson.java
@@ -1,1 +1,11 @@
 
+public class player extends character {
+  private String classType;
+  private String armorType;
+  
+  public player()
+  {
+    classType = null;
+    armorType = null;
+  }
+  public player(

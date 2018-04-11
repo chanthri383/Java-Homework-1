90e320d295d894758aad3acd8a2d8d097126a20f
chanthri383 <36742658+chanthri383@users.noreply.github.com>
2018-04-10 20:48:44 -0500

    Update Townsperson.java

--- Townsperson.java
+++ Townsperson.java
@@ -8,4 +8,13 @@ public class player extends character {
     classType = null;
     armorType = null;
   }
-  public player(
+  public player(String type_, String name_, char gender_, int hitPoints_,String classType_,String armorType_)
+  {
+    super(type_,name_,gender_,hitPoints_);
+    classType = classType_;
+    armorType = armorType_;
+  }
+  public String toString()
+  {
+    System.out.print("\n\nNPC's - Towns People");
+    System.out.print(super.toString() + System.out.format(

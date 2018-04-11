af0e0e33aa688144357915311c3cb624c4ba66f5
nm47 <Nielsmoeller9@gmail.com>
2018-04-10 20:56:36 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,4 +5,13 @@ public class Main {
     public static void main(String[] args)  {
       
 
+      
+      
+      
+      
+      
+      
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter text: ");
+    String text = input.next();
 

6799f3963bf757c0ccb5108dcd7ff770c60ba8f2
chanthri383 <36742658+chanthri383@users.noreply.github.com>
2018-04-11 19:43:31 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,1 +1,18 @@
+public class Player extends Character {
+    private String classType, armorType;
 
+    public Player()
+    {
+        classType = null;
+        armorType = null;
+    }
+    public Player(String type1, String name1, char gender1, int pointHits, String classType1,String armorType1) {
+        super(type1, name1, gender1, pointHits);
+        classType = classType1;
+        armorType = armorType1;
+    }
+    public String toString()
+    {
+        return super.toString() + String.format(); // not sure for this part
+    }
+}
